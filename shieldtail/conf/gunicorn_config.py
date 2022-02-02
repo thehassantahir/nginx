9e10312a7dfe881f5a556272d8be5eaacc42d579
thehassantahir <info.hassantahir@gmail.com>
2022-02-03 00:28:15 +0500

    gunicorn config path change

--- shieldtail/conf/gunicorn_config.py
+++ shieldtail/conf/gunicorn_config.py
@@ -1,4 +1,4 @@
-command = '/home/Desktop/nginx/django_env/bin/gunicorn'
+command = '/home/fsociety/Desktop/nginx/django_env/bin/gunicorn'
 pythonpath = '/home/Desktop/nginx/shieldtail'
 bind = '192.168.10.18'
 workers = 3
